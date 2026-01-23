f8351f6da44606d3ef8a5292ac54dd9e3377d64c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2026-01-23 14:27:53 +1000

    chore: add defaults

--- src/ui/src/services/api.ts
+++ src/ui/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Typescript based API
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL || "https://api.ftx.finance",
   headers: {
     "Content-Type": "application/json",
   },
